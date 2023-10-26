22da4ace3a9cf60372ac72e5d79684ca186c602a
Mostafa Piran <piran.m313@gmail.com>
2023-10-26 17:36:04 +0330

    Create helpers.py

---
+++ week 9/finance/helpers.py
@@ -0,0 +1,98 @@
+import csv
+import datetime
+import pytz
+import requests
+import subprocess
+import urllib
+import uuid
+from flask import redirect, render_template, session
+from functools import wraps
+import os
+import urllib.parse
+from werkzeug.security import check_password_hash
+from datetime import datetime
+
+def apology(message, code=400):
+    """Render message as an apology to user."""
+    def escape(s):
+        """
+        Escape special characters.
+
+        https://github.com/jacebrowning/memegen#special-characters
+        """
+        for old, new in [("-", "--"), (" ", "-"), ("_", "__"), ("?", "~q"),
+                         ("%", "~p"), ("#", "~h"), ("/", "~s"), ("\"", "''")]:
+            s = s.replace(old, new)
+        return s
+
+    if isinstance(message, str):
+        message = escape(message)
+    else:
+        message = "Error occured!"
+
+    return render_template("apology.html", top=code, bottom=escape(message)), code
+
+
+def login_required(f):
+    """
+    Decorate routes to require login.
+
+    http://flask.pocoo.org/docs/0.12/patterns/viewdecorators/
+    """
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if session.get("user_id") is None:
+            return redirect("/login")
+        return f(*args, **kwargs)
+    return decorated_function
+
+
+def lookup(symbol):
+    """Look up quote for symbol."""
+
+    # Prepare API request
+    symbol = symbol.upper()
+    end = datetime.datetime.now(pytz.timezone("US/Eastern"))
+    start = end - datetime.timedelta(days=7)
+
+    # Yahoo Finance API
+    url = (
+        f"https://query1.finance.yahoo.com/v7/finance/download/{urllib.parse.quote_plus(symbol)}"
+        f"?period1={int(start.timestamp())}"
+        f"&period2={int(end.timestamp())}"
+        f"&interval=1d&events=history&includeAdjustedClose=true"
+    )
+
+    # Query API
+    try:
+        response = requests.get(url, cookies={"session": str(uuid.uuid4())}, headers={"User-Agent": "python-requests", "Accept": "*/*"})
+        response.raise_for_status()
+
+        # CSV header: Date,Open,High,Low,Close,Adj Close,Volume
+        quotes = list(csv.DictReader(response.content.decode("utf-8").splitlines()))
+        quotes.reverse()
+        price = round(float(quotes[0]["Adj Close"]), 2)
+        return {
+            "name": symbol,
+            "price": price,
+            "symbol": symbol
+        }
+    except (requests.RequestException, ValueError, KeyError, IndexError):
+        return None
+
+
+def usd(value):
+    """Format value as USD."""
+    return f"${value:,.2f}"
+
+def get_time():
+    """Returns formatted local time."""
+
+    return datetime.now().strftime("%d-%m-%Y %H:%M:%S")
+
+
+def check_password(first_password, second_password):
+    """Checks the passwords to be a match. Returns a message error if they do not match."""
+
+    if not check_password_hash(first_password, second_password):
+        return apology("Passwords do not match!", code=401)
